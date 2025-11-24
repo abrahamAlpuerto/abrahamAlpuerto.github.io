d50509cf98d47b5f6ac2499c4a9f3bc1397557c9
abrahamAlpuerto <104240451+abrahamAlpuerto@users.noreply.github.com>
2025-11-24 01:02:52 +0000

    Deploying to gh-pages from @ abrahamAlpuerto/abrahamalpuerto.github.io@e30ecdab2b09666bbd04bf392d2e7362e7a13fe8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "abrahamAlpuerto/abrahamAlpuerto.github.io",
+    "data-repo": "abrahamAlpuerto/abrahamalpuerto.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
